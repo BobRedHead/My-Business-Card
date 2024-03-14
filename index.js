f0ff7cb94a4700a32e02d2fc3b9070e323f769ee
BobRedHead <144790344+BobRedHead@users.noreply.github.com>
2024-03-15 01:34:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+VANTA.FOG({
+    el: "#main",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0xff88f0
+  })
